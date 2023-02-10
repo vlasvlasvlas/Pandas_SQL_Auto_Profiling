6eff0493027a89eb784eca23fcc4943c1c1ebf52
vlasvlasvlas <vlasvlasvlas@gmail.com>
2023-02-10 16:12:14 -0300

    Update dataprofile.py
    black applied

--- scripts/dataprofile.py
+++ scripts/dataprofile.py
@@ -9,13 +9,14 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
 # fn
-def fExtract(data,dataprofiles,dataprofiles_prefix):
+def fExtract(data, dataprofiles, dataprofiles_prefix):
     name = data["name"]
-    print("-->",name)
+    print("-->", name)
     df = pd.read_sql("SELECT * from " + name, engine)
     print(df)
     profile = ProfileReport(df, title=f"{name} Profiling Report", minimal=True)
     profile.to_file(f"{dataprofiles}{dataprofiles_prefix}_{name}.html")
+
 
 # env
 project_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -33,12 +34,12 @@ cnnpwd = os.getenv("SQL_PWD")
 dataprofiles = "data-profiles/"
 
 # title prefix for output html
-dataprofiles_prefix = os.getenv("PROFILE_PREFIX")+cnndb
+dataprofiles_prefix = os.getenv("PROFILE_PREFIX") + cnndb
 
 engine = create_engine(
     "mssql+pyodbc://"
     f"{cnnuser}:{cnnpwd}@{cnnhost}:{cnnport}/{cnndb}?"
-    "driver="+os.getenv("SQL_ODBC_DRIVER")
+    "driver=" + os.getenv("SQL_ODBC_DRIVER")
 )
 
 conn = engine.connect()
@@ -51,7 +52,7 @@ tables = conn.execute(sql_tables)
 print("--> extracting views..")
 # VIEWS per row
 for table in tables:
-    fExtract(table,dataprofiles,dataprofiles_prefix+"_tb_")
+    fExtract(table, dataprofiles, dataprofiles_prefix + "_tb_")
 
 
 # get list for views
@@ -59,5 +60,4 @@ views = conn.execute(sql_views)
 print("--> extracting tables..")
 # VIEWS per row
 for view in views:
-    fExtract(view,dataprofiles,dataprofiles_prefix+"_vw_")
-
+    fExtract(view, dataprofiles, dataprofiles_prefix + "_vw_")
