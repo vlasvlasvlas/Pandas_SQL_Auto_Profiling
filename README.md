44cb33bcce2bd16809d1e44de2add0ef46c43d01
vlasvlasvlas <vlasvlasvlas@gmail.com>
2023-02-10 16:08:41 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# data_autoProfiling
- 
+# Whats this
+- automatic pandas data profiling from tables and views of a specific SQL Server Connection.
+- basically its a quick wrap-up over pandas profiling library for quick & automatic table and views output.
+
+# Structures
+- data-profiles: pandas profiling html exports. Here's where your output html profilings will be created.
+- scripts: python based script.
+
+# Scripts
+- dataprofile.py: connects to db, get list of tables and views, and for each one it generates and export pandas profiling html on output folder
+
+# Instructions
+- create virtualenv (python -m venv venv)
+- access / activate virtualenv (source venv/scripts/activate or bash script for linux based)
+- install libraries from requirements.txt (pip install -r requirements.txt)
+- edit .env.example file, rename or copy to .env
+- execute script from root path (python scripts/dataprofile.py)
+- enjoy your auto profiling!
---
+++ scripts/dataprofile.py
@@ -0,0 +1,63 @@
+from base64 import encode
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport
+from os import listdir
+import sys, os
+from datetime import datetime
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+# fn
+def fExtract(data,dataprofiles,dataprofiles_prefix):
+    name = data["name"]
+    print("-->",name)
+    df = pd.read_sql("SELECT * from " + name, engine)
+    print(df)
+    profile = ProfileReport(df, title=f"{name} Profiling Report", minimal=True)
+    profile.to_file(f"{dataprofiles}{dataprofiles_prefix}_{name}.html")
+
+# env
+project_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+load_dotenv(os.path.join(project_folder, ".env"))
+
+# from env
+# SQL conn data
+cnnhost = os.getenv("SQL_HOST")
+cnnport = os.getenv("SQL_PORT")
+cnndb = os.getenv("SQL_DBSTAGE")
+cnnuser = os.getenv("SQL_USER")
+cnnpwd = os.getenv("SQL_PWD")
+
+# html pandas profiling output folder
+dataprofiles = "data-profiles/"
+
+# title prefix for output html
+dataprofiles_prefix = os.getenv("PROFILE_PREFIX")+cnndb
+
+engine = create_engine(
+    "mssql+pyodbc://"
+    f"{cnnuser}:{cnnpwd}@{cnnhost}:{cnnport}/{cnndb}?"
+    "driver="+os.getenv("SQL_ODBC_DRIVER")
+)
+
+conn = engine.connect()
+
+sql_tables = "SELECT t.name AS name FROM sys.schemas AS s JOIN sys.tables AS t ON t.schema_id = s.schema_id ORDER BY 1"
+sql_views = "SELECT v.name as name FROM sys.views as v ORDER BY 1;"
+
+# get list for tables
+tables = conn.execute(sql_tables)
+print("--> extracting views..")
+# VIEWS per row
+for table in tables:
+    fExtract(table,dataprofiles,dataprofiles_prefix+"_tb_")
+
+
+# get list for views
+views = conn.execute(sql_views)
+print("--> extracting tables..")
+# VIEWS per row
+for view in views:
+    fExtract(view,dataprofiles,dataprofiles_prefix+"_vw_")
+
