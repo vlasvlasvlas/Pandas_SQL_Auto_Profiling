ad00bbca22a88ba83a4f25731569a8a81a0133fb
Vladimiro Bellini <vbellini@mail.austral.edu.ar>
2024-02-02 13:34:11 -0300

    nested func
    fixed

--- README.md
+++ README.md
@@ -9,10 +9,17 @@
 # Scripts
 - dataprofile.py: connects to db, get list of tables and views, and for each one it generates and export pandas profiling html on output folder
 
+## Parameteres:
+
+- dataprofiles = "data-profiles/" # folder to save the profiles
+- type = 'tables' # 'tables' or 'views
+- dataobject_prefix = 'stg_nv' # prefix of the data objects to be profiled
+- file_prefix = 'rd' # prefix for the profile file names
+
 # Instructions
 - create virtualenv (python -m venv venv)
 - access / activate virtualenv (source venv/scripts/activate or bash script for linux based)
 - install libraries from requirements.txt (pip install -r requirements.txt)
-- edit .env.example file, rename or copy to .env
+- edit .env.example file, rename or copy to .env, just for sql conn
 - execute script from root path (python scripts/dataprofile.py)
 - enjoy your auto profiling!
--- scripts/dataprofile.py
+++ scripts/dataprofile.py
@@ -8,16 +8,6 @@ from datetime import datetime
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-# fn
-def fExtract(data, dataprofiles, dataprofiles_prefix):
-    name = data["name"]
-    print("-->", name)
-    df = pd.read_sql("SELECT * from " + name, engine)
-    print(df)
-    profile = ProfileReport(df, title=f"{name} Profiling Report", minimal=True)
-    profile.to_file(f"{dataprofiles}{dataprofiles_prefix}_{name}.html")
-
-
 # env
 project_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 load_dotenv(os.path.join(project_folder, ".env"))
@@ -30,34 +20,69 @@ cnndb = os.getenv("SQL_DBSTAGE")
 cnnuser = os.getenv("SQL_USER")
 cnnpwd = os.getenv("SQL_PWD")
 
-# html pandas profiling output folder
-dataprofiles = "data-profiles/"
-
-# title prefix for output html
-dataprofiles_prefix = os.getenv("PROFILE_PREFIX") + cnndb
-
 engine = create_engine(
     "mssql+pyodbc://"
     f"{cnnuser}:{cnnpwd}@{cnnhost}:{cnnport}/{cnndb}?"
     "driver=" + os.getenv("SQL_ODBC_DRIVER")
 )
-
 conn = engine.connect()
 
-sql_tables = "SELECT t.name AS name FROM sys.schemas AS s JOIN sys.tables AS t ON t.schema_id = s.schema_id ORDER BY 1"
-sql_views = "SELECT v.name as name FROM sys.views as v ORDER BY 1;"
+# fn
+def fExtract(data, dataprofiles, dataprofiles_prefix):
+    name = data["name"]
+    print("-->", name)
+    df = pd.read_sql("SELECT * from " + name, engine)
+    print(df)
+    profile = ProfileReport(df, title=f"{name} Profiling Report", minimal=True)
+    profile.to_file(f"{dataprofiles}{dataprofiles_prefix}_{name}.html")
 
-# get list for tables
-tables = conn.execute(sql_tables)
-print("--> extracting views..")
-# VIEWS per row
-for table in tables:
-    fExtract(table, dataprofiles, dataprofiles_prefix + "_tb_")
+# function that uses fExtract to create profiles based on the data objects in the database
+# inputs: 
+# - type (table or view), 
+# - prefix (prefix of the data objects to be profiled)
+# - dataprofiles_prefix (prefix for the profile file names
+def create_profiles(type,dataobject_prefix, file_prefix):
 
+    if type == 'tables':
+        sql_tables = """
+        SELECT t.name AS name 
+        FROM sys.schemas AS s 
+        JOIN sys.tables AS t ON t.schema_id = s.schema_id 
+        WHERE 
+        t.name like '{dataobject_prefix}%' 
+        ORDER BY 1
+        """.format(dataobject_prefix=dataobject_prefix)
 
-# get list for views
-views = conn.execute(sql_views)
-print("--> extracting tables..")
-# VIEWS per row
-for view in views:
-    fExtract(view, dataprofiles, dataprofiles_prefix + "_vw_")
+        # get list for tables
+        tables = conn.execute(sql_tables)
+        print("--> extracting views..")
+
+        # TABLES per row
+        for table in tables:
+            fExtract(table, dataprofiles, file_prefix + "_Tbl_")
+
+    if type == 'views':
+        # get list for views
+
+        # specific 
+        sql_views = """
+        SELECT v.name as name FROM sys.views as v where v.name like '{dataobject_prefix}%' ORDER BY 1;
+        """.format(dataobject_prefix=dataobject_prefix)
+
+        # get list for tables
+        views = conn.execute(sql_views)
+        print("--> extracting tables..")
+
+        # VIEWS per row
+        for view in views:
+            fExtract(view, dataprofiles, file_prefix + "_Vws_")
+
+# define params:
+dataprofiles = "data-profiles/" # folder to save the profiles
+# params
+type = 'tables' # 'tables' or 'views
+dataobject_prefix = 'stg_nv' # prefix of the data objects to be profiled
+file_prefix = 'rd' # prefix for the profile file names
+
+# run
+create_profiles(type,dataobject_prefix,file_prefix)
